4f210a684a918e628a087cbd1ff370ce860c8b63
AkashNivaria <a9899akash.29@gmail.com>
2024-07-14 12:50:31 +0530

    Create mytable.sql

---
+++ Database files/mytable.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `customers` (
+  `Bill_no` int DEFAULT NULL,
+  `C_name` varchar(20) DEFAULT NULL,
+  `C_mob` varchar(12) DEFAULT NULL,
+  `P_code` int DEFAULT NULL,
+  `Qty` int DEFAULT NULL
+) 
+
+select * from customers;
